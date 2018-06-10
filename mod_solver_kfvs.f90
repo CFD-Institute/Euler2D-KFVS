a8f8d221b467d0f1a7c32bd65da04ad1b21339a6
Truong Dang <dangtruong080593@gmail.com>
2018-06-10 21:57:11 +0700

    major revision
    + update code for multi-elements airfoil (c31).
    
    + a subroutine to export pressure coefficient Cp.

--- mod_solver_kfvs.f90
+++ mod_solver_kfvs.f90
@@ -1,6 +1,7 @@
 ! Remarks:
 ! 17/03/2018: Truong replaces symmetry boundary condition with solid wall boundary condition (airfoil surface),
 !             in order to have a more meanningful solution.
+! 18/03/2018: Code is modified for the C3.1 multi-element airfoil.
 module mod_solver_kfvs
     use mod_cell_2D
     use mod_fvm_face_2D
@@ -115,17 +116,64 @@ module mod_solver_kfvs
         
         end subroutine allocate_vardummy
 !----------------------------------------------------------------------
+        subroutine allocate_vardummy_multi_elem_airfoil
+        implicit none 
+        
+        integer                    :: i
+        type(fvm_face_2D), pointer :: pfac
+        
+        !if (nb_symmetry > 0) return 
+        if (nb_paroi_solid > 0) return
+        
+        ! Allocating vardummy
+        do i = 1, nbfaces
+            pfac => faces_fvm%face_2D(i)%f
+            if (pfac%bc_typ == 1) then ! inlet
+                nb_inlet = nb_inlet + 1
+            endif
+            if (pfac%bc_typ == 2) then ! Airfoil - paroi solid
+                nb_paroi_solid = nb_paroi_solid + 1
+            endif
+            if (pfac%bc_typ == 3) then ! Airfoil - paroi solid
+                nb_paroi_solid = nb_paroi_solid + 1
+            endif
+            if (pfac%bc_typ == 4) then ! Airfoil - paroi solid
+                nb_paroi_solid = nb_paroi_solid + 1
+            endif
+        enddo 
+        
+        !if (.not. allocated(vardummy_symetrie)) then
+        !    allocate(vardummy_symetrie(1:nb_symmetry,1:8))
+        !    vardummy_symetrie = 0.0d0
+        !endif
+        
+        if (.not. allocated(vardummy_paroi_solid)) then
+            allocate(vardummy_paroi_solid(1:nb_paroi_solid,1:8))
+            vardummy_paroi_solid = 0.0d0
+        endif
+        
+        if (.not. allocated(vardummy_entree)) then
+            allocate(vardummy_entree(1:nb_inlet,1:8))
+            vardummy_entree = 0.0d0
+        endif
+        
+        !if (.not. allocated(vardummy_sortie)) then
+        !    allocate(vardummy_sortie(1:nb_outlet,1:8))
+        !    vardummy_sortie = 0.0d0
+        !endif
+        
+        end subroutine allocate_vardummy_multi_elem_airfoil
+!----------------------------------------------------------------------        
         subroutine conditions_aux_limites
         implicit none
         
         integer                    :: icel, ifac, jfac
-        integer                    :: cnt_symmetry, cnt_inlet, cnt_outlet, cnt_wall
+        integer                    :: cnt_inlet, cnt_outlet, cnt_wall
         integer                    :: idface
         real(8)                    :: un, ut
         type(cell_2D),     pointer :: pcel
         type(face),        pointer :: pfac
         
-        !cnt_symmetry = 0
         cnt_inlet    = 0
         cnt_outlet   = 0
         cnt_wall     = 0
@@ -134,14 +182,6 @@ module mod_solver_kfvs
             pcel => list_cell%cell(icel)%p
             do ifac = 1, 4
                 pfac => pcel%faces(ifac)
-                
-                !if (pfac%bc_typ == 1) then ! Airfoil - symetrie
-                !    cnt_symmetry = cnt_symmetry + 1 
-                !    vardummy_symetrie(cnt_symmetry, 1) = rho(icel)
-                !    vardummy_symetrie(cnt_symmetry, 2) = ux(icel)
-                !    vardummy_symetrie(cnt_symmetry, 3) = -uy(icel)
-                !    vardummy_symetrie(cnt_symmetry, 4) = t(icel)
-                !endif 
                 
                 if (pfac%bc_typ == 1) then ! Airfoil - paroi solid
                     cnt_wall = cnt_wall + 1 
@@ -172,6 +212,48 @@ module mod_solver_kfvs
             enddo 
         enddo 
         end subroutine conditions_aux_limites
+!----------------------------------------------------------------------        
+        subroutine conditions_aux_limites_multi_elem_airfoil
+        implicit none
+        
+        integer                    :: icel, ifac, jfac
+        integer                    :: cnt_inlet, cnt_wall
+        integer                    :: idface
+        real(8)                    :: un, ut
+        type(cell_2D),     pointer :: pcel
+        type(face),        pointer :: pfac
+        
+        cnt_inlet    = 0
+        cnt_wall     = 0
+        
+        do icel = 1, list_cell%nbelm
+            pcel => list_cell%cell(icel)%p
+            do ifac = 1, 4
+                pfac => pcel%faces(ifac)
+                
+                
+                if (pfac%bc_typ == 2 .or. pfac%bc_typ == 3 .or. pfac%bc_typ == 4) then ! Airfoil - paroi solid
+                    cnt_wall = cnt_wall + 1 
+                    idface   = pfac%idface
+                    un       =  norm_x(idface) * ux(icel) + norm_y(idface) * uy(icel)
+                    ut       = -norm_y(idface) * ux(icel) + norm_x(idface) * uy(icel)
+                    vardummy_paroi_solid(cnt_wall, 1) = rho(icel)
+                    vardummy_paroi_solid(cnt_wall, 2) = norm_x(idface) * (-un) - norm_y(idface) * ut
+                    vardummy_paroi_solid(cnt_wall, 3) = norm_y(idface) * (-un) + norm_x(idface) * ut
+                    vardummy_paroi_solid(cnt_wall, 4) = t(icel)
+                endif 
+                
+                if (pfac%bc_typ == 1) then ! Inflow
+                    cnt_inlet = cnt_inlet + 1 
+                    vardummy_entree(cnt_inlet, 1) = 0.2969689477d-4
+                    vardummy_entree(cnt_inlet, 2) = 1059.458022d0
+                    vardummy_entree(cnt_inlet, 3) = 0.0d0
+                    vardummy_entree(cnt_inlet, 4) = 1295.646765d0
+                endif 
+                
+            enddo 
+        enddo 
+        end subroutine conditions_aux_limites_multi_elem_airfoil        
 !----------------------------------------------------------------------
         !--- calcul des quantités dérivées
         subroutine calcul_derived_quantities
@@ -183,10 +265,6 @@ module mod_solver_kfvs
         a     = rho / (8.0d0 * b**3)
         e     = 0.5d0 * rho * (ux**2 + uy**2) + 3.0d0 /2.0d0 * rho * r_gaz * t
         
-        !vardummy_symetrie(:, 5) = vardummy_symetrie(:, 1) * r_gaz * vardummy_symetrie(:, 4)
-        !vardummy_symetrie(:, 7) = sqrt(3.0d0 * r_gaz * vardummy_symetrie(:, 4))
-        !vardummy_symetrie(:, 6) = vardummy_symetrie(:, 1) / (8.0d0 * vardummy_symetrie(:, 7)**3)
-        !vardummy_symetrie(:, 8) = 0.5d0 * vardummy_symetrie(:, 1) * (vardummy_symetrie(:, 2)**2 + vardummy_symetrie(:, 3)**2) + 3.0d0 /2.0d0 * vardummy_symetrie(:, 1) * r_gaz * vardummy_symetrie(:, 4)
         
         vardummy_paroi_solid(:, 5) = vardummy_paroi_solid(:, 1) * r_gaz * vardummy_paroi_solid(:, 4)
         vardummy_paroi_solid(:, 7) = sqrt(3.0d0 * r_gaz * vardummy_paroi_solid(:, 4))
@@ -205,6 +283,29 @@ module mod_solver_kfvs
         vardummy_sortie(:, 8) = 0.5d0 * vardummy_sortie(:, 1) * (vardummy_sortie(:, 2)**2 + vardummy_sortie(:, 3)**2) + 3.0d0 /2.0d0 * vardummy_sortie(:, 1) * r_gaz * vardummy_sortie(:, 4)
         
         end subroutine calcul_derived_quantities
+!----------------------------------------------------------------------
+        subroutine calcul_derived_quantities_multi_elem_airfoil
+        implicit none
+        
+        r_gaz = 1.3806503d-23 / 0.663d-25
+        p     = rho * r_gaz * t
+        b     = sqrt(3.0d0 * r_gaz * t)
+        a     = rho / (8.0d0 * b**3)
+        e     = 0.5d0 * rho * (ux**2 + uy**2) + 3.0d0 /2.0d0 * rho * r_gaz * t
+        
+        
+        vardummy_paroi_solid(:, 5) = vardummy_paroi_solid(:, 1) * r_gaz * vardummy_paroi_solid(:, 4)
+        vardummy_paroi_solid(:, 7) = sqrt(3.0d0 * r_gaz * vardummy_paroi_solid(:, 4))
+        vardummy_paroi_solid(:, 6) = vardummy_paroi_solid(:, 1) / (8.0d0 * vardummy_paroi_solid(:, 7)**3)
+        vardummy_paroi_solid(:, 8) = 0.5d0 * vardummy_paroi_solid(:, 1) * (vardummy_paroi_solid(:, 2)**2 + vardummy_paroi_solid(:, 3)**2) + &
+            & 3.0d0 /2.0d0 * vardummy_paroi_solid(:, 1) * r_gaz * vardummy_paroi_solid(:, 4)
+        
+        vardummy_entree(:, 5) = vardummy_entree(:, 1) * r_gaz * vardummy_entree(:, 4)
+        vardummy_entree(:, 7) = sqrt(3.0d0 * r_gaz * vardummy_entree(:, 4))
+        vardummy_entree(:, 6) = vardummy_entree(:, 1) / (8.0d0 * vardummy_entree(:, 7)**3)
+        vardummy_entree(:, 8) = 0.5d0 * vardummy_entree(:, 1) * (vardummy_entree(:, 2)**2 + vardummy_entree(:, 3)**2) + 3.0d0 /2.0d0 * vardummy_entree(:, 1) * r_gaz * vardummy_entree(:, 4)
+        
+        end subroutine calcul_derived_quantities_multi_elem_airfoil
 !----------------------------------------------------------------------
         !--- calcul du vecteur des quantités conservatives
         subroutine calcul_conservative_vector
@@ -272,12 +373,11 @@ module mod_solver_kfvs
         use mod_struct_to_array, only: lr_cell
         implicit none 
         integer                    :: ifac, icel, fac
-        integer                    :: cnt_symmetry, cnt_inlet, cnt_outlet, cnt_wall
+        integer                    :: cnt_inlet, cnt_outlet, cnt_wall
         type(cell_2D), pointer     :: pcel
         type(face), pointer        :: pfac
         type(fvm_face_2D), pointer :: pfac_fvm
         
-        !cnt_symmetry = 0
         cnt_inlet    = 0
         cnt_outlet   = 0
         cnt_wall     = 0
@@ -287,12 +387,6 @@ module mod_solver_kfvs
             pcel => list_cell%cell(icel)%p
             do ifac = 1, 4
                 pfac => pcel%faces(ifac)
-                !if (pfac%bc_typ == 1) then ! Airfoil - symetrie
-                !    cnt_symmetry = cnt_symmetry + 1
-                !    fac = pfac%idface
-                !    lr_cell(fac, 1) = icel
-                !    lr_cell(fac, 2) = cnt_symmetry ! dummy cell for symmetry bc
-                !endif
                 
                 if (pfac%bc_typ == 1) then ! Airfoil - paroi solid
                     cnt_wall = cnt_wall + 1
@@ -327,6 +421,51 @@ module mod_solver_kfvs
         enddo 
         end subroutine assign_lr_cell
 !----------------------------------------------------------------------
+        subroutine assign_lr_cell_multi_elem_airfoil
+        use mod_struct_to_array, only: lr_cell
+        implicit none 
+        integer                    :: ifac, icel, fac
+        integer                    :: cnt_inlet, cnt_wall
+        type(cell_2D), pointer     :: pcel
+        type(face), pointer        :: pfac
+        type(fvm_face_2D), pointer :: pfac_fvm
+        
+        cnt_inlet    = 0
+        cnt_wall     = 0
+        
+        ! Create left cell - right cell table for boundary faces
+        do icel = 1, list_cell%nbelm
+            pcel => list_cell%cell(icel)%p
+            do ifac = 1, 4
+                pfac => pcel%faces(ifac)
+                
+                if (pfac%bc_typ == 2 .or. pfac%bc_typ == 3 .or. pfac%bc_typ == 4) then ! Airfoil - paroi solid
+                    cnt_wall = cnt_wall + 1
+                    fac = pfac%idface
+                    lr_cell(fac, 1) = icel
+                    lr_cell(fac, 2) = cnt_wall ! dummy cell for solid wall bc
+                endif
+                
+                if (pfac%bc_typ == 1) then ! Inflow
+                    cnt_inlet = cnt_inlet + 1 
+                    fac = pfac%idface
+                    lr_cell(fac, 1) = icel
+                    lr_cell(fac, 2) = cnt_inlet ! dummy cell for inlet bc
+                endif 
+                
+            enddo 
+        enddo 
+        
+        ! Create left cell - right cell table for internal faces
+        do ifac = 1, nbfaces
+            pfac_fvm => faces_fvm%face_2D(ifac)%f
+            if (associated(pfac_fvm%left_cell) .and. associated(pfac_fvm%right_cell)) then 
+                lr_cell(ifac, 1) = pfac_fvm%left_cell%ident
+                lr_cell(ifac, 2) = pfac_fvm%right_cell%ident
+            endif
+        enddo 
+        end subroutine assign_lr_cell_multi_elem_airfoil
+!----------------------------------------------------------------------
         subroutine calcul_flux
         use mod_flux_kfvs
         use mod_struct_to_array
@@ -352,15 +491,6 @@ module mod_solver_kfvs
                 flux(ifac,:)   = len_norm(ifac) * (flux_plus(:) + flux_minus(:))
             endif 
             
-            !if (bc_typ(ifac) == 1) then !symmetry
-            !    flux_plus(:)   = fluxp(rho(left_cell), ux(left_cell), uy(left_cell), & 
-            !    & e(left_cell), p(left_cell), t(left_cell), a(left_cell), b(left_cell), & 
-            !    & norm_x(ifac), norm_y(ifac))
-            !    flux_minus(:)  = fluxm(vardummy_symetrie(right_cell,1), vardummy_symetrie(right_cell,2), vardummy_symetrie(right_cell,3), & 
-            !    & vardummy_symetrie(right_cell,8), vardummy_symetrie(right_cell,5), vardummy_symetrie(right_cell,4), vardummy_symetrie(right_cell,6), vardummy_symetrie(right_cell,7), & 
-            !    & norm_x(ifac), norm_y(ifac))
-            !    flux(ifac,:)   = len_norm(ifac) * (flux_plus(:) + flux_minus(:))
-            !endif
             
             if (bc_typ(ifac) == 1) then !solid wall
                 flux_plus(:)   = fluxp(rho(left_cell), ux(left_cell), uy(left_cell), & 
@@ -394,6 +524,56 @@ module mod_solver_kfvs
         enddo 
         
         end subroutine calcul_flux
+!----------------------------------------------------------------------
+        subroutine calcul_flux_multi_elem_airfoil
+        use mod_flux_kfvs
+        use mod_struct_to_array
+        implicit none 
+        integer :: ifac, left_cell, right_cell
+        real(8) :: flux_plus(1:4), flux_minus(1:4)
+        
+        if (.not. allocated(flux)) then
+            allocate(flux(1:nbfaces,1:4))
+        endif
+        
+        do ifac = 1, nbfaces
+            left_cell  = lr_cell(ifac,1)
+            right_cell = lr_cell(ifac,2)
+            
+            if (bc_typ(ifac) == 0) then
+                flux_plus(:)   = fluxp(rho(left_cell), ux(left_cell), uy(left_cell), & 
+                & e(left_cell), p(left_cell), t(left_cell), a(left_cell), b(left_cell), & 
+                & norm_x(ifac), norm_y(ifac))
+                flux_minus(:)  = fluxm(rho(right_cell), ux(right_cell), uy(right_cell), & 
+                & e(right_cell), p(right_cell), t(right_cell), a(right_cell), b(right_cell), & 
+                & norm_x(ifac), norm_y(ifac))
+                flux(ifac,:)   = len_norm(ifac) * (flux_plus(:) + flux_minus(:))
+            endif 
+            
+            
+            if (bc_typ(ifac) == 2 .or. bc_typ(ifac) == 3 .or. bc_typ(ifac) == 4) then !solid wall
+                flux_plus(:)   = fluxp(rho(left_cell), ux(left_cell), uy(left_cell), & 
+                & e(left_cell), p(left_cell), t(left_cell), a(left_cell), b(left_cell), & 
+                & norm_x(ifac), norm_y(ifac))
+                flux_minus(:)  = fluxm(vardummy_paroi_solid(right_cell,1), vardummy_paroi_solid(right_cell,2), vardummy_paroi_solid(right_cell,3), & 
+                & vardummy_paroi_solid(right_cell,8), vardummy_paroi_solid(right_cell,5), vardummy_paroi_solid(right_cell,4), vardummy_paroi_solid(right_cell,6), &
+                & vardummy_paroi_solid(right_cell,7), norm_x(ifac), norm_y(ifac))
+                flux(ifac,:)   = len_norm(ifac) * (flux_plus(:) + flux_minus(:))
+            endif
+            
+            if (bc_typ(ifac) == 1) then ! Inflow
+                flux_plus(:)   = fluxp(rho(left_cell), ux(left_cell), uy(left_cell), & 
+                & e(left_cell), p(left_cell), t(left_cell), a(left_cell), b(left_cell), & 
+                & norm_x(ifac), norm_y(ifac))
+                flux_minus(:)  = fluxm(vardummy_entree(right_cell,1), vardummy_entree(right_cell,2), vardummy_entree(right_cell,3), & 
+                & vardummy_entree(right_cell,8), vardummy_entree(right_cell,5), vardummy_entree(right_cell,4), vardummy_entree(right_cell,6), vardummy_entree(right_cell,7), & 
+                & norm_x(ifac), norm_y(ifac))
+                flux(ifac,:)   = len_norm(ifac) * (flux_plus(:) + flux_minus(:))
+            endif
+            
+        enddo 
+        
+        end subroutine calcul_flux_multi_elem_airfoil
 !----------------------------------------------------------------------
         subroutine calcul_rhs
         use mod_struct_to_array
@@ -446,5 +626,53 @@ module mod_solver_kfvs
         uy(1:nbelm)  = vect_u(1:nbelm,3) / rho(1:nbelm)
         t(1:nbelm)   = 2.0d0/(3.0d0 * r_gaz * rho(1:nbelm)) *(vect_u(1:nbelm,4)-0.5d0*rho(1:nbelm)*(ux(1:nbelm)**2+uy(1:nbelm)**2))
         end subroutine calcul_rho_ux_uy_t
+!----------------------------------------------------------------------
+        subroutine write_pressure_coefficient(iter)                ! not accurate           
+        implicit none 
+        
+        integer, intent(in):: iter
+        
+        
+        real(8), parameter :: rhoref =    0.2d-4
+        real(8), parameter :: velref =  500.0d0
+        real(8), parameter :: temref = 1000.0d0 
+        character(7)       :: citer
+        character(300)     :: foutput
+        
+        integer                :: ifac, left_cell, ilocfac
+        type(cell_2D), pointer :: pcel
+        type(face),    pointer :: pfac
+        real(8)                :: cp, pinf, ploc
+        real(8)                :: vinf
+        
+        
+        vinf = velref
+        pinf = rhoref * r_gaz * temref
+        
+        write(citer,'(I7.7)') iter
+        foutput = 'Cp_'//trim(fname)//'_'//trim(citer)//'.txt'
+        
+        open(unit = 20, file = foutput, status = 'replace', position = 'append')
+        write(20,*) 'X, Y, -Cp'
+        
+        do ifac = 1, nbfaces
+            if (bc_typ(ifac) == 1) then                            ! solid wall
+                left_cell  = lr_cell(ifac,1)
+                pcel => list_cell%cell(left_cell)%p
+                
+                ploc = rho(left_cell) * r_gaz * t(left_cell)
+                cp   = 2.0d0 * (ploc - pinf) / (pinf * velref**2 )
+                
+                do ilocfac = 1, 4
+                    pfac => pcel%faces(ilocfac)
+                    if (pfac%bc_typ == 1) then 
+                        write(20,*) pfac%centroid%x, pfac%centroid%y, -cp
+                    endif 
+                enddo 
+            endif 
+        enddo 
+        
+        close(unit = 20)
+        end subroutine write_pressure_coefficient
 !----------------------------------------------------------------------
 end module
--- solver.f90
+++ solver.f90
@@ -7,16 +7,26 @@ end subroutine solver
 subroutine solver_kfvs
     use mod_solver_kfvs
     use mod_struct_to_array
+    use mod_read_gmsh,     only: fname
     implicit none 
     integer :: n
     
     call struct_to_array 
     
     call donnee_initiale
-    call allocate_vardummy
-    call conditions_aux_limites
-    call assign_lr_cell 
-    call calcul_derived_quantities 
+    
+    if (fname(1:3) == 'c31') then 
+        call allocate_vardummy_multi_elem_airfoil
+        call conditions_aux_limites_multi_elem_airfoil
+        call assign_lr_cell_multi_elem_airfoil 
+        call calcul_derived_quantities_multi_elem_airfoil 
+    else
+        call allocate_vardummy
+        call conditions_aux_limites
+        call assign_lr_cell 
+        call calcul_derived_quantities 
+    endif
+    
     call calcul_conservative_vector 
     
     call timestep 
@@ -26,13 +36,17 @@ subroutine solver_kfvs
     nmax=floor(tmax/dt)
     
     ! debug
-    nmax = 500000
+    nmax = 5000000
     
     !--- evolution
     do n=1,nmax
         
         !-- calcul des flux
-        call calcul_flux 
+        if (fname(1:3) == 'c31') then 
+            call calcul_flux_multi_elem_airfoil 
+        else
+            call calcul_flux
+        endif 
         
         !-- iteration en temps
         call calcul_rhs
@@ -45,15 +59,24 @@ subroutine solver_kfvs
         call calcul_rho_ux_uy_t
         
         !-- mise a jour cl
-        call conditions_aux_limites
+        if (fname(1:3) == 'c31') then 
+            call conditions_aux_limites_multi_elem_airfoil 
+        else
+            call conditions_aux_limites
+        endif 
         
         !-- mise à jour des quantités dérivées
-        call calcul_derived_quantities
+        if (fname(1:3) == 'c31') then 
+            call calcul_derived_quantities_multi_elem_airfoil 
+        else
+            call calcul_derived_quantities
+        endif 
         
         !-- sauvegarde resultats (format vtk, lisible par Paraview)
-        if (mod(n,10000) == 0) then
+        if (mod(n,1000) == 0) then
             write(*,*) 'Writing solution file at iteration ', n, '...'
             call write_solution_vtk(n)
+            call write_pressure_coefficient(n)
         endif 
     enddo 
     
